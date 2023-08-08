acc1f8ce4656d7c64904dde97a74d3303e0938b0
@khalilullah.alfaath21 <khalilullah.alfaath21@gmail.com>
2023-08-08 08:29:09 +0700

    implement UserVerify Model

--- app/Models/UserVerify.php
+++ app/Models/UserVerify.php
@@ -8,4 +8,16 @@ use Illuminate\Database\Eloquent\Model;
 class UserVerify extends Model
 {
     use HasFactory;
+
+    protected $table = 'users_verify';
+
+    protected $fillable = [
+        'user_id',
+        'token'
+    ];
+
+    public function user()
+    {
+        return $this->belongsTo(User::class);
+    }
 }
